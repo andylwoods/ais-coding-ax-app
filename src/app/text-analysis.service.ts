67889abf363ee08081ac537d0c23b394e3cbd0b2
aw <aw@hothpts.com.au>
2026-01-10 00:54:11 +0000

    xml updates

--- src/app/text-analysis.service.ts
+++ src/app/text-analysis.service.ts
@@ -9,6 +9,19 @@ export class TextAnalysisService {
   private http = inject(HttpClient);
 
   analyzeText(text: string, outputFormat: 'json' | 'xml'): Observable<any> {
-    return this.http.post('https://ais-api-backend-1000379470338.us-central1.run.app/api/TextAnalysis', { text, outputFormat: outputFormat });
+    const body = { text, outputFormat };
+
+    if (outputFormat === 'xml') {
+      return this.http.post(
+        'https://ais-api-backend-1000379470338.us-central1.run.app/api/TextAnalysis',
+        body,
+        { responseType: 'text' }
+      );
+    } else {
+      return this.http.post(
+        'https://ais-api-backend-1000379470338.us-central1.run.app/api/TextAnalysis',
+        body
+      );
+    }
   }
 }
